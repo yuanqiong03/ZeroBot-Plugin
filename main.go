0b0199512bc8e42d80f9b6e6fc3dccbd7f7824a2
YQ233 <101623975+yuanqiong03@users.noreply.github.com>
2022-11-10 16:58:32 +0800

    rename

--- main.go
+++ main.go
@@ -233,7 +233,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	sus = append(sus, 974464175)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 87654321)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
